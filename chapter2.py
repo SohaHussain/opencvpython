2b92d222036034e10b5e4b3b1015eca1ca2b8738
Soha <soha00hussain@gmail.com>
2021-07-24 00:13:02 +0530

    basic functions

--- chapter2.py
+++ chapter2.py
@@ -2,3 +2,15 @@
 
 import cv2
 
+# loading image
+img = cv2.imread('resources/lena.png')
+
+# now we will convert this image into gray scale
+
+# cvtColor converts our image into different color spaces
+# BGR is for blue green red
+imggray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+# displaying image
+cv2.imshow("gray image",imggray)
+cv2.waitKey(0)
