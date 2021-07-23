32cc24d714b18bbeffcdd9046a9c62204fa82263
Soha <soha00hussain@gmail.com>
2021-07-23 22:15:43 +0530

    reading and displaying image

--- chapter1.py
+++ chapter1.py
@@ -1,7 +1,13 @@
 #chapter 1: read images , videos and webcam
 import cv2
 
-img=cv2.imread("resources/lena.png")  # function to read images // in brackets add path of image
-cv2.imshow("output",img)  # function to display image . 1st argument is name of window and second is image
+img = cv2.imread("resources/lena.png")  # function to read images // in brackets add path of image
+cv2.imshow("output", img)  # function to display image . 1st argument is name of window and second is image
+# the image will appear for just a few milliseconds
+# to delay display time
+cv2.waitKey(0) # 0 is for infinite time
+
+
+
 
 
